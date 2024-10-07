06da2679098934a61deb8f70062286865287b5d0
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-07 02:30:58 -0400

    some more configuration for the use of SSL

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
-    readonly VITE_USE_SECURE_API: string
-    readonly VITE_API_SECURE_URL: string
-    readonly VITE_API_URL: string
-    readonly VITE_USE_SECURE_API: boolean
-  }
+  readonly VITE_APP_TITLE: string
+  readonly VITE_USE_SECURE_API: string
+  readonly VITE_API_SECURE_URL: string
+  readonly VITE_API_URL: string
+  readonly VITE_USE_SECURE_API: boolean
+}
 
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as fs from 'fs';
 // https://vitejs.dev/config/
 
-
 const server = {};
 
-if ('TRUE' === process.env.VITE_USE_SECURE_API) {
-  Object.assign(server,{
-    https:{
-      key: fs.readFileSync('./.cert/key.pem'),
-      cert: fs.readFileSync('./.cert/cert.pem'),
-    },
+export default (mode: string) => {
+  const loadedEnv = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  if ('TRUE' === loadedEnv.VITE_USE_SECURE_API) {
+    Object.assign(server, {
+      https: {
+        key: fs.readFileSync('./.cert/key.pem'),
+        cert: fs.readFileSync('./.cert/cert.pem'),
+      },
+    });
+  }
+
+  return defineConfig({
+    server,
+    plugins: [react()],
   });
-}
-export default defineConfig({
-  server,
-  plugins: [react()],
-});
+};
